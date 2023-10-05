6cf5f325c55bd1905d993edfbec5505dbf69425e
lesiamon <lesiamon.mpoe@student.moringaschool.com>
2023-10-05 21:39:38 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-
 // Write your code here
+
+const num1 = 31;
+const num2 = 2;
+const num3 = 16;
+const num4 = 6;
+
+const multiply = num1 * num2;
+const random = Math.floor(Math.random() * 10) + 1;
+const mod = num3 % num4;
+const max = Math.max(5, 10, 15, 20);
+
+console.log(multiply); // Output: 62
+console.log(random);  // Output: A random integer greater than 0
+console.log(mod);     // Output: 4
+console.log(max);     // Output: 20
